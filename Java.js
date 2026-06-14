348dc7da8c5dab012b35ae32db6d8985ae727e0b
Stevo10-Coder <202168287+Stevo10-coder@users.noreply.github.com>
2026-06-14 20:57:01 +0200

    add dynamic grid generation via change button

--- Java.js
+++ Java.js
@@ -1,10 +1,38 @@
 
 const container = document.querySelector("#container");
 const feld = document.getElementById("container");
+const restartBtn = document.querySelector("#restartBtn");
+const changeBtn = document.getElementById("changeBtn");
 
-for (let i = 0; i < 256; i++) {
+
+
+changeBtn.addEventListener("click", function() {
+    const question = Number(prompt("Wie viele Quadrate möchtest du?"));
+    if (question <= 100) {
+        container.innerHTML = "";
+        createGrid(question);
+    } else if (question > 100) {
+        alert("Error!");
+    } else {
+        alert("Gib eine Zahl zwischen 1 und 100 ein!");
+    }
+
+    
+
+    
+});
+
+document.getElementById("restartBtn").addEventListener("click", () => {
+    window.location.reload();
+});
+
+function createGrid(zahl) {
+    let ergebnis = zahl * zahl;
+for (let i = 0; i < ergebnis; i++) {
     const quadrat = document.createElement("div");
     quadrat.classList.add("quadrat");
+    quadrat.style.width = 100 / zahl + "%";
+    quadrat.style.height = 100 / zahl + "%";
 
      quadrat.addEventListener("mouseenter", () => {
      quadrat.classList.add("hovered");
@@ -13,7 +41,9 @@ for (let i = 0; i < 256; i++) {
 
     container.appendChild(quadrat);
 }
+}
 
+createGrid(16);
 
 
 
